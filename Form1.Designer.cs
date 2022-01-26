d6e68957fc8702e136fd79787acee36d78412ef4
Gospodarikova_VS <gospodarikova@corepartners.ru>
2022-01-26 21:45:45 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace ImageProcessing
+{
+    partial class ExecImagesForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExecImagesForm));
+            this.ControlButtonsPanel = new System.Windows.Forms.Panel();
+            this.ProcessImageButton = new System.Windows.Forms.Button();
+            this.KindOfProcessingComboBox = new System.Windows.Forms.ComboBox();
+            this.SaveResultImage = new System.Windows.Forms.Button();
+            this.LoadImageButton = new System.Windows.Forms.Button();
+            this.ImagesSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.InputImagePictureBox = new System.Windows.Forms.PictureBox();
+            this.OutputImagePictureBox = new System.Windows.Forms.PictureBox();
+            this.PeriodicalPatternButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ControlButtonsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagesSplitContainer)).BeginInit();
+            this.ImagesSplitContainer.Panel1.SuspendLayout();
+            this.ImagesSplitContainer.Panel2.SuspendLayout();
+            this.ImagesSplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.InputImagePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputImagePictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ControlButtonsPanel
+            // 
+            this.ControlButtonsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ControlButtonsPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ControlButtonsPanel.Controls.Add(this.ProcessImageButton);
+            this.ControlButtonsPanel.Controls.Add(this.KindOfProcessingComboBox);
+            this.ControlButtonsPanel.Controls.Add(this.SaveResultImage);
+            this.ControlButtonsPanel.Controls.Add(this.LoadImageButton);
+            this.ControlButtonsPanel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ControlButtonsPanel.Location = new System.Drawing.Point(0, 0);
+            this.ControlButtonsPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ControlButtonsPanel.Name = "ControlButtonsPanel";
+            this.ControlButtonsPanel.Size = new System.Drawing.Size(880, 46);
+            this.ControlButtonsPanel.TabIndex = 0;
+            // 
+            // ProcessImageButton
+            // 
+            this.ProcessImageButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ProcessImageButton.Location = new System.Drawing.Point(440, 8);
+            this.ProcessImageButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ProcessImageButton.Name = "ProcessImageButton";
+            this.ProcessImageButton.Size = new System.Drawing.Size(155, 27);
+            this.ProcessImageButton.TabIndex = 3;
+            this.ProcessImageButton.Text = "Proccess image";
+            this.ProcessImageButton.UseVisualStyleBackColor = true;
+            this.ProcessImageButton.Click += new System.EventHandler(this.ProcessImageButton_Click);
+            // 
+            // KindOfProcessingComboBox
+            // 
+            this.KindOfProcessingComboBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.KindOfProcessingComboBox.FormattingEnabled = true;
+            this.KindOfProcessingComboBox.Location = new System.Drawing.Point(282, 8);
+            this.KindOfProcessingComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.KindOfProcessingComboBox.Name = "KindOfProcessingComboBox";
+            this.KindOfProcessingComboBox.Size = new System.Drawing.Size(155, 29);
+            this.KindOfProcessingComboBox.TabIndex = 2;
+            // 
+            // SaveResultImage
+            // 
+            this.SaveResultImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveResultImage.Location = new System.Drawing.Point(755, 8);
+            this.SaveResultImage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SaveResultImage.Name = "SaveResultImage";
+            this.SaveResultImage.Size = new System.Drawing.Size(114, 27);
+            this.SaveResultImage.TabIndex = 1;
+            this.SaveResultImage.Text = "Save result";
+            this.SaveResultImage.UseVisualStyleBackColor = true;
+            this.SaveResultImage.Click += new System.EventHandler(this.SaveResultImage_Click);
+            // 
+            // LoadImageButton
+            // 
+            this.LoadImageButton.Location = new System.Drawing.Point(10, 8);
+            this.LoadImageButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.LoadImageButton.Name = "LoadImageButton";
+            this.LoadImageButton.Size = new System.Drawing.Size(114, 27);
+            this.LoadImageButton.TabIndex = 0;
+            this.LoadImageButton.Text = "Load image";
+            this.LoadImageButton.UseVisualStyleBackColor = true;
+            this.LoadImageButton.Click += new System.EventHandler(this.LoadImageButton_Click);
+            // 
+            // ImagesSplitContainer
+            // 
+            this.ImagesSplitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ImagesSplitContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ImagesSplitContainer.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.ImagesSplitContainer.Location = new System.Drawing.Point(0, 50);
+            this.ImagesSplitContainer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ImagesSplitContainer.Name = "ImagesSplitContainer";
+            // 
+            // ImagesSplitContainer.Panel1
+            // 
+            this.ImagesSplitContainer.Panel1.AutoScroll = true;
+            this.ImagesSplitContainer.Panel1.Controls.Add(this.InputImagePictureBox);
+            // 
+            // ImagesSplitContainer.Panel2
+            // 
+            this.ImagesSplitContainer.Panel2.AutoScroll = true;
+            this.ImagesSplitContainer.Panel2.Controls.Add(this.OutputImagePictureBox);
+            this.ImagesSplitContainer.Size = new System.Drawing.Size(880, 500);
+            this.ImagesSplitContainer.SplitterDistance = 436;
+            this.ImagesSplitContainer.TabIndex = 1;
+            // 
+            // InputImagePictureBox
+            // 
+            this.InputImagePictureBox.Location = new System.Drawing.Point(-1, -1);
+            this.InputImagePictureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.InputImagePictureBox.Name = "InputImagePictureBox";
+            this.InputImagePictureBox.Size = new System.Drawing.Size(438, 491);
+            this.InputImagePictureBox.TabIndex = 0;
+            this.InputImagePictureBox.TabStop = false;
+            // 
+            // OutputImagePictureBox
+            // 
+            this.OutputImagePictureBox.Location = new System.Drawing.Point(0, -1);
+            this.OutputImagePictureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.OutputImagePictureBox.Name = "OutputImagePictureBox";
+            this.OutputImagePictureBox.Size = new System.Drawing.Size(439, 491);
+            this.OutputImagePictureBox.TabIndex = 0;
+            this.OutputImagePictureBox.TabStop = false;
+            // 
+            // PeriodicalPatternButton
+            // 
+            this.PeriodicalPatternButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.PeriodicalPatternButton.Location = new System.Drawing.Point(12, 13);
+            this.PeriodicalPatternButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PeriodicalPatternButton.Name = "PeriodicalPatternButton";
+            this.PeriodicalPatternButton.Size = new System.Drawing.Size(142, 32);
+            this.PeriodicalPatternButton.TabIndex = 4;
+            this.PeriodicalPatternButton.Text = "Periodical pattern";
+            this.PeriodicalPatternButton.UseVisualStyleBackColor = true;
+            this.PeriodicalPatternButton.Click += new System.EventHandler(this.PeriodicalPatternButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.PeriodicalPatternButton);
+            this.panel1.Location = new System.Drawing.Point(0, 555);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(880, 57);
+            this.panel1.TabIndex = 5;
+            // 
+            // ExecImagesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(880, 611);
+            this.Controls.Add(this.ControlButtonsPanel);
+            this.Controls.Add(this.ImagesSplitContainer);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(1262, 820);
+            this.MinimumSize = new System.Drawing.Size(702, 460);
+            this.Name = "ExecImagesForm";
+            this.Text = "ExecImages";
+            this.ControlButtonsPanel.ResumeLayout(false);
+            this.ImagesSplitContainer.Panel1.ResumeLayout(false);
+            this.ImagesSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ImagesSplitContainer)).EndInit();
+            this.ImagesSplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.InputImagePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputImagePictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ControlButtonsPanel;
+        private System.Windows.Forms.SplitContainer ImagesSplitContainer;
+        private System.Windows.Forms.Button SaveResultImage;
+        private System.Windows.Forms.Button LoadImageButton;
+        private System.Windows.Forms.Button ProcessImageButton;
+        private System.Windows.Forms.ComboBox KindOfProcessingComboBox;
+        private System.Windows.Forms.PictureBox InputImagePictureBox;
+        private System.Windows.Forms.PictureBox OutputImagePictureBox;
+        private System.Windows.Forms.Label InputMatrixLabel;
+        private System.Windows.Forms.Button PeriodicalPatternButton;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageProcessing
+{
+    public partial class ExecImagesForm : Form
+    {
+        Dictionary<string, string> KindOfProcessingImagesDictionary;
+
+        // Add some elements to the dictionary. There are no
+        // duplicate keys, but some of the values are duplicates.
+
+        public ExecImagesForm()
+        {
+            InitializeComponent();
+            initComboBox();
+            hideStepsAfterFirst();
+        }
+
+        private void initComboBox()
+        {
+            KindOfProcessingComboBox.DropDownHeight = 150;
+
+            KindOfProcessingImagesDictionary = new Dictionary<string, string>();
+            KindOfProcessingImagesDictionary.Add("copy", "Copy");
+            KindOfProcessingImagesDictionary.Add("gray", "Gray colors");
+            KindOfProcessingImagesDictionary.Add("convolution", "Convolution");
+
+            string[] values = KindOfProcessingImagesDictionary.Values.ToArray<string>();
+            KindOfProcessingComboBox.Items.AddRange(values);
+        }
+
+        private void hideStepsAfterFirst()
+        {
+            KindOfProcessingComboBox.Hide();
+            ProcessImageButton.Hide();
+            SaveResultImage.Hide();
+        }
+
+        private void LoadImageButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+
+            dialog.Title = "Open Image";
+            //dialog.Filter = "bmp files (*.bmp)|*.bmp";
+
+            if (dialog.ShowDialog() == DialogResult.OK && dialog.CheckPathExists && dialog.CheckFileExists)
+            {
+                Bitmap inputOriginalBitmap = new Bitmap(dialog.FileName);
+                /*if (inputOriginalBitmap.Width > 500)
+                {
+                    inputOriginalBitmap = inputOriginalBitmap.Clone(new Rectangle(0, 0, 500, inputOriginalBitmap.Height), inputOriginalBitmap.PixelFormat);
+                }
+                if (inputOriginalBitmap.Height > 655)
+                {
+                    inputOriginalBitmap = inputOriginalBitmap.Clone(new Rectangle(0, 0, inputOriginalBitmap.Width, 655), inputOriginalBitmap.PixelFormat);
+                }*/
+                InputImagePictureBox.Image = inputOriginalBitmap;
+                OutputImagePictureBox.Image = null;
+                KindOfProcessingComboBox.Show();
+                ProcessImageButton.Show();
+                SaveResultImage.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("Error with upload chosen file");
+                KindOfProcessingComboBox.Hide();
+                ProcessImageButton.Hide();
+            }
+            dialog.Dispose();
+        }
+
+        private void SaveResultImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var dialog = new OpenFileDialog();
+
+                dialog.Title = "Save Image";
+                //dialog.Filter = "bmp files (*.bmp)|*.bmp";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    OutputImagePictureBox.Image.Save(dialog.FileName);
+
+                }
+                dialog.Dispose();
+
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Error with save result image. Error messge: " + exp.Message);
+            }
+        }
+
+        private void ProcessImageButton_Click(object sender, EventArgs e)
+        {
+            if (KindOfProcessingComboBox.SelectedItem != null)
+            {
+                string operation = KindOfProcessingComboBox.SelectedItem.ToString();
+                int i = -1;
+                string str = "";
+                if (KindOfProcessingImagesDictionary.TryGetValue("copy", out str) && operation == str)
+                {
+                    i = 0;
+                }
+                else if (KindOfProcessingImagesDictionary.TryGetValue("gray", out str) && operation == str)
+                {
+                    i = 1;
+                }
+                else if (KindOfProcessingImagesDictionary.TryGetValue("convolution", out str) && operation == str)
+                {
+                    i = 2;
+                }
+                switch (i)
+                {
+                    case 0:
+                        this.copyImage();
+                        break;
+                    case 1:
+                        this.colorImageGray();
+                        break;
+                    case 2:
+                        this.startConvolutionImageWithMatrix();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Firstly choose an option in a dropdown");
+            }
+        }
+
+        private void copyImage()
+        {
+            OutputImagePictureBox.Image = InputImagePictureBox.Image;
+        }
+
+        private void colorImageGray()
+        {
+            Bitmap inputBitmap = (Bitmap)InputImagePictureBox.Image;
+            Bitmap outputBitmap = new Bitmap(inputBitmap.Width, inputBitmap.Height);
+            int height = InputImagePictureBox.Image.Height;
+            int width = InputImagePictureBox.Image.Width;
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    Color origColor = inputBitmap.GetPixel(w, h);
+                    int averageBrightness = (origColor.R + origColor.G + origColor.B) / 3;
+                    outputBitmap.SetPixel(w, h, Color.FromArgb(averageBrightness, averageBrightness, averageBrightness));
+                }
+            }
+            OutputImagePictureBox.Image = outputBitmap;
+        }
+
+        private void startConvolutionImageWithMatrix()
+        {
+            Bitmap inputBitmap = (Bitmap)InputImagePictureBox.Image;
+            Bitmap outputBitmap = new Bitmap(inputBitmap.Width, inputBitmap.Height);
+            double u = 0, v = 0.5;
+            int height = InputImagePictureBox.Image.Height;
+            int width = InputImagePictureBox.Image.Width;
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    Color origColor = inputBitmap.GetPixel(w, h);
+                    int averageBrightness = (origColor.R + origColor.G + origColor.B) / 3;
+                    int editBrightness = Convert.ToInt32(127 + 50 * Math.Cos(u * h + v * w));
+                    // Make processed image
+                    Color editColor = Color.FromArgb(editBrightness, editBrightness, editBrightness);
+                    outputBitmap.SetPixel(w, h, editColor);
+                }
+            }
+            OutputImagePictureBox.Image = outputBitmap;
+        }
+
+        private void getPeriodicalPattern()
+        {
+            int height = 500;
+            int width = 400;
+            int n = 2;
+            double[] u = new double[n];
+            double[] v = new double[n];
+            u[0] = 0.2; v[0] = -0.4;
+            u[1] = -0.2; v[1] = 0.4;
+
+            Bitmap inputBitmap = new Bitmap(width, height);
+            Bitmap outputBitmap = new Bitmap(width, height);
+
+            int heightOfPie = height / n;
+            int widthOfPie = width / n;
+
+            int conversionAddition = 50;
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    int finalBrightness = 0;
+                    finalBrightness = Convert.ToInt32((Math.Cos(u[0] * h + v[0] * w)+ Math.Cos(u[0] * h + v[1] * w)+ Math.Cos(u[1] * h + v[0] * w)+ Math.Cos(u[1] * h + v[1] * w)) * conversionAddition + conversionAddition * 2);
+                    finalBrightness = finalBrightness < 0 ? 0 : (finalBrightness > 255 ? 255 : finalBrightness);
+                    Color finalColor = Color.FromArgb(finalBrightness, finalBrightness, finalBrightness);
+                    outputBitmap.SetPixel(w, h, finalColor);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int h = i * heightOfPie; h < heightOfPie * i + heightOfPie; h++)
+                    {
+                        for (int w = j * widthOfPie; w < widthOfPie * j + widthOfPie; w++)
+                        {
+                            int editBrightness = Convert.ToInt32(conversionAddition * 2 + conversionAddition * Math.Cos(u[i] * h + v[j] * w));
+                            // Make processed image
+                            Color editColor = Color.FromArgb(editBrightness, editBrightness, editBrightness);
+                            inputBitmap.SetPixel(w, h, editColor);
+                        }
+                    }
+                }
+            }
+
+            InputImagePictureBox.Image = inputBitmap;
+            OutputImagePictureBox.Image = outputBitmap;
+        }
+
+        private void PeriodicalPatternButton_Click(object sender, EventArgs e)
+        {
+            getPeriodicalPattern();
+        }
+    }
+}
